dc63fb772218d9e09a3a36c1043df92cccf9bcfe
Giovanny Cholca <byroncholca@gmail.com>
2026-04-11 14:22:38 -0500

    configuracion de la cadena conexion

--- appNetCore/app.ecCliente.api/app.ecCliente.api/Program.cs
+++ appNetCore/app.ecCliente.api/app.ecCliente.api/Program.cs
@@ -1,3 +1,6 @@
+using app.ecCliente.dataAccess.context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//LA CADENA DE CONEXION ESTA EN EL appsettings.json
+//CON EL SIGUIENTA LINEA OBTENEMOS LA CADENA DE CONEXION A SQL SERVER
+var conSqlServer = builder.Configuration.GetConnectionString("BDDSqlServer")!;
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(conSqlServer);
+    options.LogTo(Console.WriteLine, LogLevel.Information).EnableSensitiveDataLogging();
+});
+
+
+
 
 var app = builder.Build();
 
